21624e5f7646412e53a9d637c0ab818890598046
thejustlazy <53787313+thejustlazy@users.noreply.github.com>
2020-04-01 21:46:10 +0600

    s

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String HGK()
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            string response = wc.DownloadString("http://www.cbr.ru/scripts/XML_daily.asp");
+            string Rate = System.Text.RegularExpressions.Regex.Match(response, @"Гонконгский доллар</Name><Value>([0-9]+.[0-9]+)</Value>").Groups[1].Value;
+            return Rate;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            textBox1.Text = "1 RUB = " + HGK() + " HGK";
+        }
+    }
+}
